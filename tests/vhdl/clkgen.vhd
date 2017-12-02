7eca5ef87a958b58948e1c232a8eafc8f48335dc
Fabian Schuiki <fabian@schuiki.ch>
2017-12-02 11:54:58 +0100

    vhdl: add clkgen test
    Add a simple test case which currently does not compile.

---
+++ tests/vhdl/clkgen.vhd
@@ -0,0 +1,22 @@
+package pkg is
+	type BIT is range 0 to 1;
+end package;
+
+entity clkgen is
+	port (CK: out pkg.BIT);
+end;
+
+architecture behav of clkgen is
+begin
+	p_clkgen: process
+	begin
+		CK <= 0;
+		wait for 10 ns;
+		for I in 0 to 3 loop
+			CK <= 1;
+			wait for 5 ns;
+			CK <= 0;
+			wait for 5 ns;
+		end loop;
+	end process;
+end;
