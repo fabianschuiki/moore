c1e0abb51e51da341bbd703989f7acc43ce6108d
Fabian Schuiki <fabian@schuiki.ch>
2018-01-11 19:35:02 +0100

    ci: don't upload if cargo doc fails
    Move the `cargo doc` invocation into the `ci/doc-upload.sh` script. When
    rustdoc fails the script will abort rather than uploading an empty
    documentation to GitHub pages.
    
    This fixes #22.

--- ci/doc-upload.sh
+++ ci/doc-upload.sh
@@ -5,11 +5,19 @@
 
 set -e
 
+# Generate documentaiton.
+cargo doc
+
+# Assemble comit message.
 COMMIT=`git rev-parse --short HEAD`
 MSG="Documentation for moore ($COMMIT)"
 
+# Create redirect to moore documentation.
 echo "<meta http-equiv=refresh content=0;url=moore/index.html>" > target/doc/index.html
 
+# Clone ghp-import if needed and import target/doc directory.
 [ -d ghp-import ] || git clone https://github.com/davisp/ghp-import
 ghp-import/ghp_import.py -n -m "$MSG" target/doc
+
+# Push to GitHub.
 git push -fq https://${GH_TOKEN}@github.com/fabianschuiki/moore.git gh-pages
