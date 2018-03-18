f6325398ec71afa9745200ce0d210285edabf47d
Fabian Schuiki <fabian@schuiki.ch>
2018-03-18 12:49:18 +0100

    vhdl: add overhauled integer type
    Add the struct `IntegerType` to the `ty2` module, which is a lightweight
    wrapper around the new more fundamental `Range` struct. The latter
    encapsulates the semantics of ranges in VHDL with `to` or `downto`
    direction, as well as left/right and lower/upper bounds. Integer types
    simply deref to a range that can be checked for its length, null, and
    bounds.

--- src/vhdl/ty2/types.rs
+++ src/vhdl/ty2/types.rs
@@ -4,6 +4,10 @@
 
 use std::fmt::{self, Debug, Display};
 use std::iter::{once, repeat};
+use std::ops::{Add, Sub, Deref};
+
+pub use num::{BigInt, BigRational};
+use num::One;
 
 use common::name::{get_name_table, Name};
 
@@ -41,7 +45,8 @@ pub trait Type: Debug + Display {
 pub enum AnyType<'t> {
     /// An enumeration type.
     Enum(&'t EnumType),
-    // integer
+    /// An integer type.
+    Integer(&'t IntegerType),
     // float
     // physical
     // array
@@ -60,19 +65,22 @@ pub enum AnyType<'t> {
 impl<'t> Type for AnyType<'t> {
     fn is_scalar(&self) -> bool {
         match *self {
-            AnyType::Enum(t) => t.is_scalar(),
+            AnyType::Enum(t)    => t.is_scalar(),
+            AnyType::Integer(t) => t.is_scalar(),
         }
     }
 
     fn is_discrete(&self) -> bool {
         match *self {
-            AnyType::Enum(t) => t.is_discrete(),
+            AnyType::Enum(t)    => t.is_discrete(),
+            AnyType::Integer(t) => t.is_discrete(),
         }
     }
 
     fn is_numeric(&self) -> bool {
         match *self {
-            AnyType::Enum(t) => t.is_numeric(),
+            AnyType::Enum(t)    => t.is_numeric(),
+            AnyType::Integer(t) => t.is_numeric(),
         }
     }
 
@@ -84,7 +92,8 @@ impl<'t> Type for AnyType<'t> {
 impl<'t> Display for AnyType<'t> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            AnyType::Enum(t) => Display::fmt(t, f),
+            AnyType::Enum(t)    => Display::fmt(t, f),
+            AnyType::Integer(t) => Display::fmt(t, f),
         }
     }
 }
@@ -92,7 +101,8 @@ impl<'t> Display for AnyType<'t> {
 impl<'t> Debug for AnyType<'t> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            AnyType::Enum(t) => Debug::fmt(t, f),
+            AnyType::Enum(t)    => Debug::fmt(t, f),
+            AnyType::Integer(t) => Debug::fmt(t, f),
         }
     }
 }
@@ -104,8 +114,6 @@ impl<'t, T: Type> From<&'t T> for AnyType<'t> {
 }
 
 /// An enumeration type.
-///
-/// See IEEE 1076-2008 section 5.2.2.
 #[derive(Debug)]
 pub struct EnumType {
     /// The enumeration literals.
@@ -114,6 +122,8 @@ pub struct EnumType {
 
 impl EnumType {
     /// Create a new enumeration type.
+    ///
+    /// # Example
     ///
     /// ```
     /// use moore_vhdl::ty2::{Type, EnumType};
@@ -194,6 +204,276 @@ impl Display for EnumLiteral {
         match *self {
             EnumLiteral::Ident(n) => write!(f, "{}", n),
             EnumLiteral::Char(c) => write!(f, "'{}'", c),
+        }
+    }
+}
+
+/// An integer type.
+#[derive(Debug)]
+pub struct IntegerType {
+    /// The range of values.
+    range: Range<BigInt>,
+}
+
+impl IntegerType {
+    /// Create a new integer type.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{Type, IntegerType, Range, RangeDir, BigInt};
+    ///
+    /// let a = IntegerType::new(Range::ascending(0, 42));
+    /// let b = IntegerType::new(Range::descending(42, 0));
+    ///
+    /// assert_eq!(format!("{}", a), "0 to 42");
+    /// assert_eq!(format!("{}", a.as_any()), "0 to 42");
+    /// assert_eq!(format!("{}", b), "42 downto 0");
+    /// assert_eq!(format!("{}", b.as_any()), "42 downto 0");
+    /// assert_eq!(a.dir(), RangeDir::To);
+    /// assert_eq!(b.dir(), RangeDir::Downto);
+    /// assert_eq!(a.len(), BigInt::from(43));
+    /// assert_eq!(b.len(), BigInt::from(43));
+    /// ```
+    pub fn new(range: Range<BigInt>) -> IntegerType {
+        IntegerType {
+            range: range,
+        }
+    }
+}
+
+impl Type for IntegerType {
+    fn is_scalar(&self) -> bool { true }
+    fn is_discrete(&self) -> bool { true }
+    fn is_numeric(&self) -> bool { true }
+    fn as_any(&self) -> AnyType { AnyType::Integer(self) }
+}
+
+impl Display for IntegerType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.range)
+    }
+}
+
+impl Deref for IntegerType {
+    type Target = Range<BigInt>;
+    fn deref(&self) -> &Range<BigInt> {
+        &self.range
+    }
+}
+
+/// A directed range of values.
+///
+/// `Range<T>` has the same semantics as ranges in VHDL. They have a direction
+/// associated with them, and left and right bounds. The range may be a null
+/// range if the lower bound is greater than or equal to the upper bound.
+#[derive(Debug, PartialEq, Eq)]
+pub struct Range<T> {
+    /// The direction.
+    dir: RangeDir,
+    /// The left bound.
+    left: T,
+    /// The right bound.
+    right: T,
+}
+
+impl<T: Ord + One> Range<T> where for<'a> &'a T: Add<Output=T> + Sub<Output=T> {
+    /// Create an ascending range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::IntegerRange;
+    ///
+    /// let r = IntegerRange::ascending(0, 42);
+    ///
+    /// assert_eq!(format!("{}", r), "0 to 42");
+    /// ```
+    pub fn ascending<L,R>(left: L, right: R) -> Range<T> where T: From<L> + From<R> {
+        Range {
+            dir: RangeDir::To,
+            left: left.into(),
+            right: right.into(),
+        }
+    }
+
+    /// Create a descending range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::IntegerRange;
+    ///
+    /// let r = IntegerRange::descending(42, 0);
+    ///
+    /// assert_eq!(format!("{}", r), "42 downto 0");
+    /// ```
+    pub fn descending<L,R>(left: L, right: R) -> Range<T> where T: From<L> + From<R> {
+        Range {
+            dir: RangeDir::Downto,
+            left: left.into(),
+            right: right.into(),
+        }
+    }
+
+    /// Return the direction of the range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{IntegerRange, RangeDir};
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::descending(42, 0);
+    ///
+    /// assert_eq!(a.dir(), RangeDir::To);
+    /// assert_eq!(b.dir(), RangeDir::Downto);
+    /// ```
+    pub fn dir(&self) -> RangeDir {
+        self.dir
+    }
+
+    /// Return the left bound of the range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{IntegerRange, BigInt};
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::descending(42, 0);
+    ///
+    /// assert_eq!(a.left(), &BigInt::from(0));
+    /// assert_eq!(b.left(), &BigInt::from(42));
+    /// ```
+    pub fn left(&self) -> &T {
+        &self.left
+    }
+
+    /// Return the right bound of the range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{IntegerRange, BigInt};
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::descending(42, 0);
+    ///
+    /// assert_eq!(a.right(), &BigInt::from(42));
+    /// assert_eq!(b.right(), &BigInt::from(0));
+    /// ```
+    pub fn right(&self) -> &T {
+        &self.right
+    }
+
+    /// Return the lower bound of the range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{IntegerRange, BigInt};
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::descending(42, 0);
+    ///
+    /// assert_eq!(a.lower(), &BigInt::from(0));
+    /// assert_eq!(b.lower(), &BigInt::from(0));
+    /// ```
+    pub fn lower(&self) -> &T {
+        match self.dir {
+            RangeDir::To => &self.left,
+            RangeDir::Downto => &self.right,
+        }
+    }
+
+    /// Return the upper bound of the range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{IntegerRange, BigInt};
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::descending(42, 0);
+    ///
+    /// assert_eq!(a.upper(), &BigInt::from(42));
+    /// assert_eq!(b.upper(), &BigInt::from(42));
+    /// ```
+    pub fn upper(&self) -> &T {
+        match self.dir {
+            RangeDir::To => &self.right,
+            RangeDir::Downto => &self.left,
+        }
+    }
+
+    /// Return true if the range is a null range.
+    ///
+    /// A null range has its lower bound greater than or equal to its upper
+    /// bound, and thus also a length of 0 or lower.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::IntegerRange;
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::ascending(42, 0);
+    ///
+    /// assert_eq!(a.is_null(), false);
+    /// assert_eq!(b.is_null(), true);
+    /// ```
+    pub fn is_null(&self) -> bool {
+        self.lower() >= self.upper()
+    }
+
+    /// Return the length of the range.
+    ///
+    /// The length of a range is defined as `upper + 1 - lower`. The result may
+    /// be negative, indicating that the range is a null range.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{IntegerRange, BigInt};
+    ///
+    /// let a = IntegerRange::ascending(0, 42);
+    /// let b = IntegerRange::ascending(42, 0);
+    ///
+    /// assert_eq!(a.len(), BigInt::from(43));
+    /// assert_eq!(b.len(), BigInt::from(-41));
+    /// ```
+    pub fn len(&self) -> T {
+        &(self.upper() + &One::one()) - self.lower()
+    }
+}
+
+impl<T: Display> Display for Range<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.left, self.dir, self.right)
+    }
+}
+
+/// A range of integer values.
+pub type IntegerRange = Range<BigInt>;
+
+/// A range of real values.
+pub type RealRange = Range<BigRational>;
+
+/// A range direction.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum RangeDir {
+    /// An ascending range.
+    To,
+    /// A descending range.
+    Downto,
+}
+
+impl Display for RangeDir {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RangeDir::To => write!(f, "to"),
+            RangeDir::Downto => write!(f, "downto"),
         }
     }
 }
