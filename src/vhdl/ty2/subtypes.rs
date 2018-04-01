928917c230efc8a6e6cc72d8088a84769773cc44
Fabian Schuiki <fabian@schuiki.ch>
2018-04-01 09:33:37 +0200

    vhdl: add preliminary enum subtype
    Add an initial `EnumSubtype` to the new `ty2`. It is based on a more
    general `ScalarSubtype` which imposes a range constraint on any of the
    scalar types. Type aliases exist for enum, integer, floating-point, and
    physical types.
    
    The subtype currently implements the `Subtype` trait, which does not do
    much. I should probably find a way to unify types and subtypes under the
    `Type` trait to facilitate ease of use and provide a common interface.

--- src/vhdl/ty2/subtypes.rs
+++ src/vhdl/ty2/subtypes.rs
@@ -4,6 +4,81 @@
 
 use std::fmt::{self, Debug, Display};
 
+use ty2::{Type, TypeMark, Range, BigInt, EnumType, IntegerType, FloatingType, PhysicalType};
+
 /// An interface for dealing with subtypes.
 pub trait Subtype: Debug + Display {
 }
+
+/// A subtype of a scalar type.
+///
+/// Scalar types may be subtyped by a range constraint.
+#[derive(Debug)]
+pub struct ScalarSubtype<'t, T: Type + 't, C> {
+    #[allow(dead_code)]
+    resfn: Option<usize>,
+    mark: &'t TypeMark<'t>,
+    base: &'t T,
+    con: Range<C>,
+}
+
+/// A subtype of an enumeration type.
+pub type EnumSubtype<'t> = ScalarSubtype<'t, EnumType, usize>;
+
+impl<'t> EnumSubtype<'t> {
+    /// Create a new enumeration subtype.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use moore_vhdl::ty2::{Type, TypeMark, EnumType, EnumSubtype, Range};
+    /// use moore_vhdl::common::name::get_name_table;
+    ///
+    /// let ty = EnumType::new(vec![
+    ///     "first".into(),
+    ///     "second".into(),
+    ///     '0'.into(),
+    ///     '1'.into(),
+    /// ]);
+    /// let tm = TypeMark::new(
+    ///     get_name_table().intern("MY_TYPE", false),
+    ///     &ty,
+    /// );
+    /// let subty = EnumSubtype::new(&tm, Range::ascending(1usize, 2usize));
+    ///
+    /// assert_eq!(format!("{}", subty), "MY_TYPE range second to '0'");
+    /// ```
+    pub fn new(mark: &'t TypeMark, con: Range<usize>) -> EnumSubtype<'t> {
+        let base = mark.as_any().unwrap_enum();
+        assert!(*con.upper() <= base.len());
+        EnumSubtype {
+            resfn: None,
+            mark: mark,
+            base: base,
+            con: con,
+        }
+    }
+}
+
+impl<'t> Subtype for EnumSubtype<'t> {
+}
+
+impl<'t> Display for EnumSubtype<'t> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{} range {} {} {}",
+            self.mark,
+            self.base.literal(*self.con.left()),
+            self.con.dir(),
+            self.base.literal(*self.con.right()),
+        )
+	}
+}
+
+/// A subtype of an integer type.
+pub type IntegerSubtype<'t> = ScalarSubtype<'t, IntegerType, BigInt>;
+
+/// A subtype of a floating-point type.
+pub type FloatingSubtype<'t> = ScalarSubtype<'t, FloatingType, f64>;
+
+/// A subtype of a physical type.
+pub type PhysicalSubtype<'t> = ScalarSubtype<'t, PhysicalType, BigInt>;
