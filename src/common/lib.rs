7b79f28c846263f6325dfc16b8c0bc051b76a66a
Fabian Schuiki <fabian@schuiki.ch>
2018-04-15 13:29:56 +0200

    common: add session context
    Add a `SessionContext` trait that encapsulates diagnostic emission and
    checking if certain options are set in the session. This should simplify
    contexts further down the line in the compiler.

--- src/common/lib.rs
+++ src/common/lib.rs
@@ -3,10 +3,10 @@
 //! This crate contains the fundamental utilities used to by the rest of the
 //! moore compiler.
 
-extern crate rustc_serialize;
-extern crate memmap;
 #[macro_use]
 extern crate bitflags;
+extern crate memmap;
+extern crate rustc_serialize;
 
 pub mod errors;
 pub mod lexer;
@@ -22,28 +22,40 @@ use errors::{DiagBuilder2, DiagEmitter, Severity};
 use std::cell::Cell;
 
 pub struct Session {
-	pub opts: SessionOptions,
-	/// Whether any error diagnostics were produced.
-	pub failed: Cell<bool>,
+    pub opts: SessionOptions,
+    /// Whether any error diagnostics were produced.
+    pub failed: Cell<bool>,
 }
 
 impl Session {
-	/// Create a new session.
-	pub fn new() -> Session {
-		Session {
-			opts: Default::default(),
-			failed: Cell::new(false),
-		}
-	}
+    /// Create a new session.
+    pub fn new() -> Session {
+        Session {
+            opts: Default::default(),
+            failed: Cell::new(false),
+        }
+    }
 }
 
 impl DiagEmitter for Session {
-	fn emit(&self, diag: DiagBuilder2) {
-		if diag.severity >= Severity::Error {
-			self.failed.set(true);
-		}
-		eprintln!("{}", diag);
-	}
+    fn emit(&self, diag: DiagBuilder2) {
+        if diag.severity >= Severity::Error {
+            self.failed.set(true);
+        }
+        eprintln!("{}", diag);
+    }
+}
+
+impl SessionContext for Session {
+    fn has_verbosity(&self, verb: Verbosity) -> bool {
+        self.opts.verbosity.contains(verb)
+    }
+}
+
+/// Access session options and emit diagnostics.
+pub trait SessionContext: DiagEmitter {
+    /// Check if a verbosity option is set.
+    fn has_verbosity(&self, verb: Verbosity) -> bool;
 }
 
 /// A set of options for a session.
@@ -52,23 +64,23 @@ impl DiagEmitter for Session {
 /// in order to configure the execution of the program.
 #[derive(Debug, Default)]
 pub struct SessionOptions {
-	pub ignore_duplicate_defs: bool,
-	/// Print a trace of scoreboard invocations for debugging purposes.
-	pub trace_scoreboard: bool,
-	/// The verbosity options.
-	pub verbosity: Verbosity,
+    pub ignore_duplicate_defs: bool,
+    /// Print a trace of scoreboard invocations for debugging purposes.
+    pub trace_scoreboard: bool,
+    /// The verbosity options.
+    pub verbosity: Verbosity,
 }
 
 bitflags! {
-	/// A set of verbosity options for a session.
-	///
-	/// These flags control how much information the compiler emits.
-	#[derive(Default)]
-	pub struct Verbosity: u8 {
-		const TYPES         = 0b00001;
-		const EXPR_TYPES    = 0b00010;
-		const TYPE_CONTEXTS = 0b00100;
-		const TYPECK        = 0b01000;
-		const NAMES         = 0b10000;
-	}
+    /// A set of verbosity options for a session.
+    ///
+    /// These flags control how much information the compiler emits.
+    #[derive(Default)]
+    pub struct Verbosity: u8 {
+        const TYPES         = 0b00001;
+        const EXPR_TYPES    = 0b00010;
+        const TYPE_CONTEXTS = 0b00100;
+        const TYPECK        = 0b01000;
+        const NAMES         = 0b10000;
+    }
 }
