4f6a29cb6c782d896ae237b8cfae3aae02336a4f
Fabian Schuiki <fabian@schuiki.ch>
2019-01-05 12:46:54 +0100

    cli: remove compile subcommand
    Remove the `compile` subcommand which is no longer relevant.

--- src/bin/moore.rs
+++ src/bin/moore.rs
@@ -63,34 +63,6 @@ fn main() {
                 .global(true),
         )
         .subcommand(
-            SubCommand::with_name("compile")
-                .arg(
-                    Arg::with_name("inc")
-                        .short("I")
-                        .value_name("DIR")
-                        .help("Adds a search path for SystemVerilog includes")
-                        .multiple(true)
-                        .takes_value(true)
-                        .number_of_values(1),
-                )
-                .arg(
-                    Arg::with_name("preproc")
-                        .short("E")
-                        .help("Only preprocess input files"),
-                )
-                .arg(
-                    Arg::with_name("dump_ast")
-                        .long("dump-ast")
-                        .help("Dump the parsed abstract syntax tree"),
-                )
-                .arg(
-                    Arg::with_name("INPUT")
-                        .help("The input file to use")
-                        .required(true)
-                        .index(1),
-                ),
-        )
-        .subcommand(
             SubCommand::with_name("elaborate")
                 .arg(
                     Arg::with_name("NAME")
@@ -196,118 +168,11 @@ fn main() {
     }
 
     // Invoke the compiler.
-    if let Some(m) = matches.subcommand_matches("compile") {
-        compile(m);
-    } else if let Some(m) = matches.subcommand_matches("elaborate") {
+    if let Some(m) = matches.subcommand_matches("elaborate") {
         session.opts.ignore_duplicate_defs = m.is_present("ignore_duplicate_defs");
         elaborate(m, &session);
     } else if let Some(m) = matches.subcommand_matches("score") {
         score(&session, m);
-    }
-}
-
-fn compile(matches: &ArgMatches) {
-    // Prepare a list of include paths.
-    let include_paths: Vec<_> = match matches.values_of("inc") {
-        Some(args) => args.map(|x| std::path::Path::new(x)).collect(),
-        None => Vec::new(),
-    };
-    let filename = matches.value_of("INPUT").unwrap();
-
-    // Detect the file type.
-    let language = match Path::new(&filename).extension().and_then(|s| s.to_str()) {
-        Some("sv") | Some("svh") => Language::SystemVerilog,
-        Some("v") => Language::Verilog,
-        Some("vhd") | Some("vhdl") => Language::Vhdl,
-        Some(_) => panic!("Unrecognized extension of file '{}'", filename),
-        None => panic!("Unable to determine language of file '{}'", filename),
-    };
-
-    let sm = source::get_source_manager();
-    let source = match sm.open(&filename) {
-        Some(s) => s,
-        None => panic!("Unable to open input file '{}'", filename),
-    };
-
-    match language {
-        Language::SystemVerilog | Language::Verilog => {
-            // Run the input file only through the SystemVerilog preprocessor if
-            // so requested on the command line.
-            let preproc = svlog::preproc::Preprocessor::new(source, &include_paths);
-            if matches.is_present("preproc") {
-                for res in preproc {
-                    print!("{}", res.unwrap().1.extract());
-                }
-            } else {
-                let lexer = svlog::lexer::Lexer::new(preproc);
-                let ast = match svlog::parser::parse(lexer) {
-                    Ok(x) => x,
-                    Err(()) => std::process::exit(1),
-                };
-
-                // TODO: Serialize the parsed AST to disk. If a file is parsed
-                // multiple times, the tree of the previous iteration needs to
-                // be removed. Upon elaboration, it must be possible to
-                // efficiently query the AST for identifiers and check in which
-                // subtree the node lives.
-
-                let key = {
-                    let mut s = sha1::Sha1::new();
-                    s.update(filename.as_bytes());
-                    s.digest().to_string()
-                };
-                svlog::store::store_items(".moore", &key, ast).unwrap();
-            }
-        }
-        Language::Vhdl => {
-            let ast = match vhdl::syntax::parse(source) {
-                Ok(x) => x,
-                Err(()) => std::process::exit(1),
-            };
-            if matches.is_present("dump_ast") {
-                println!("{:#?}", ast);
-            }
-
-            // Be very stupid for now, and don't write anything to disk. Just
-            // keep on using this AST and pretend that it was loaded from disk.
-            // TODO: Fix this once we have a proper way of persisting build data
-            // to disk.
-
-            // 1) renumber AST nodes and build scope and symbol table
-            // let mut symtbl = vhdl::symtbl::SymTbl::new();
-            // {
-            //  use name::get_name_table;
-            //  use source::{Spanned, INVALID_SPAN};
-            //  use self::vhdl::symtbl::{DefName, Def};
-            //  let mut renum = vhdl::pass::renumber::Renumberer::new(&mut symtbl);
-            //  let name = get_name_table().intern("work", false); // could be something other than "work"
-            //  let work = get_name_table().intern("work", false);
-            //  let lib_id = renum.symtbl.get_library_id(name);
-            //  renum.push_scope(lib_id);
-            //  // This declares the library under its actual name, but also the
-            //  // alias "work". Maybe we should move this somewhere more
-            //  // appropriate.
-            //  renum.declare(Spanned::new(DefName::Ident(name), INVALID_SPAN), Def::Lib(lib_id));
-            //  if name != work {
-            //      renum.declare(Spanned::new(DefName::Ident(work), INVALID_SPAN), Def::Lib(lib_id));
-            //  }
-            //  ast = ast.into_iter().map(|n| renum.fold_design_unit(n)).collect();
-            //  renum.pop_scope();
-            //  println!("renumbered {} design units", ast.len());
-            // }
-
-            // // 2) resolve names and map to HIR
-            // {
-            //  use name::get_name_table;
-            //  let mut lower = vhdl::pass::lower::Lowerer::new(&mut symtbl);
-            //  let name = get_name_table().intern("work", false); // could be something other than "work"
-            //  let lib_id = lower.symtbl.get_library_id(name);
-            //  lower.push_scope(lib_id);
-            //  let hir: Vec<_> = ast.into_iter().map(|n| lower.fold_design_unit(n)).collect();
-            //  lower.pop_scope();
-            //  println!("lowered design units: {:#?}", hir);
-            // }
-        }
     }
 }
 
