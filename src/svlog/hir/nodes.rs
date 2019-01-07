92ea3706cc7d326cdc41560c1d18a3cffe5a02e6
Fabian Schuiki <fabian@schuiki.ch>
2019-01-07 22:11:37 +0100

    hir: comment out unused nodes

--- src/svlog/hir/nodes.rs
+++ src/svlog/hir/nodes.rs
@@ -212,47 +212,47 @@ impl HasDesc for TypeParam {
     }
 }
 
-/// An interface.
-pub struct Interface {
-    pub id: NodeId,
-    pub name: Name,
-    pub span: Span,
-    pub lifetime: ast::Lifetime,
-    pub ports: Vec<Port>,
-    pub params: Vec<ast::ParamDecl>,
-    pub body: HierarchyBody,
-}
+// /// An interface.
+// pub struct Interface {
+//     pub id: NodeId,
+//     pub name: Name,
+//     pub span: Span,
+//     pub lifetime: ast::Lifetime,
+//     pub ports: Vec<Port>,
+//     pub params: Vec<ast::ParamDecl>,
+//     pub body: HierarchyBody,
+// }
 
-/// A package.
-pub struct Package {
-    pub name: Name,
-    pub span: Span,
-    pub lifetime: ast::Lifetime,
-    pub body: HierarchyBody,
-}
+// /// A package.
+// pub struct Package {
+//     pub name: Name,
+//     pub span: Span,
+//     pub lifetime: ast::Lifetime,
+//     pub body: HierarchyBody,
+// }
 
-/// A hierarchy body represents the contents of a module, interface, or package.
-/// Generate regions and nested modules introduce additional bodies. The point
-/// of hierarchy bodies is to take a level of the design hierarchy and group all
-/// declarations by type, rather than having them in a single array in
-/// declaration order.
-pub struct HierarchyBody {
-    pub procs: Vec<ast::Procedure>,
-    pub nets: Vec<ast::NetDecl>,
-    pub vars: Vec<ast::VarDecl>,
-    pub assigns: Vec<ast::ContAssign>,
-    pub params: Vec<ast::ParamDecl>,
-    pub insts: Vec<ast::Inst>,
-    pub genreg: Vec<HierarchyBody>,
-    pub genvars: Vec<ast::GenvarDecl>,
-    pub genfors: Vec<GenerateFor>,
-    pub genifs: Vec<GenerateIf>,
-    pub gencases: Vec<ast::GenerateCase>,
-    pub classes: Vec<ast::ClassDecl>, // TODO: Make this an HIR node, since it contains hierarchy items
-    pub subroutines: Vec<ast::SubroutineDecl>, // TODO: Make this an HIR node
-    pub asserts: Vec<ast::Assertion>,
-    pub typedefs: Vec<ast::Typedef>,
-}
+// /// A hierarchy body represents the contents of a module, interface, or package.
+// /// Generate regions and nested modules introduce additional bodies. The point
+// /// of hierarchy bodies is to take a level of the design hierarchy and group all
+// /// declarations by type, rather than having them in a single array in
+// /// declaration order.
+// pub struct HierarchyBody {
+//     pub procs: Vec<ast::Procedure>,
+//     pub nets: Vec<ast::NetDecl>,
+//     pub vars: Vec<ast::VarDecl>,
+//     pub assigns: Vec<ast::ContAssign>,
+//     pub params: Vec<ast::ParamDecl>,
+//     pub insts: Vec<ast::Inst>,
+//     pub genreg: Vec<HierarchyBody>,
+//     pub genvars: Vec<ast::GenvarDecl>,
+//     pub genfors: Vec<GenerateFor>,
+//     pub genifs: Vec<GenerateIf>,
+//     pub gencases: Vec<ast::GenerateCase>,
+//     pub classes: Vec<ast::ClassDecl>, // TODO: Make this an HIR node, since it contains hierarchy items
+//     pub subroutines: Vec<ast::SubroutineDecl>, // TODO: Make this an HIR node
+//     pub asserts: Vec<ast::Assertion>,
+//     pub typedefs: Vec<ast::Typedef>,
+// }
 
 /// A module or interface port.
 #[derive(Debug, PartialEq, Eq)]
@@ -285,58 +285,55 @@ impl HasDesc for Port {
     }
 }
 
-/// A port slice refers to a port declaration within the module. It consists of
-/// the name of the declaration and a list of optional member and index accesses
-/// that select individual parts of the declaration.
-#[derive(Debug)]
-pub struct PortSlice {
-    pub id: NodeId,
-    pub name: Name,
-    pub span: Span,
-    pub selects: Vec<PortSelect>,
-    pub dir: ast::PortDir,
-    pub kind: ast::PortKind,
-    pub ty: Option<ast::Type>,
-    pub dims: Vec<ast::TypeDim>,
-}
+// /// A port slice refers to a port declaration within the module. It consists of
+// /// the name of the declaration and a list of optional member and index accesses
+// /// that select individual parts of the declaration.
+// #[derive(Debug)]
+// pub struct PortSlice {
+//     pub id: NodeId,
+//     pub name: Name,
+//     pub span: Span,
+//     pub selects: Vec<PortSelect>,
+//     pub dir: ast::PortDir,
+//     pub kind: ast::PortKind,
+//     pub ty: Option<ast::Type>,
+//     pub dims: Vec<ast::TypeDim>,
+// }
 
-#[derive(Debug)]
-pub enum PortSelect {
-    Member(Span, Name),
-    Index(Span, Expr),
-}
+// #[derive(Debug)]
+// pub enum PortSelect {
+//     Member(Span, Name),
+//     Index(Span, Expr),
+// }
 
-pub struct PortDecl {
-    pub dir: ast::PortDir,
-}
+// pub struct PortDecl {
+//     pub dir: ast::PortDir,
+// }
 
 // pub enum PortDir {
 
 // }
 
-#[derive(Debug)]
-pub struct Expr;
+// pub struct GenerateBlock {
+//     pub span: Span,
+//     pub label: Option<Name>,
+//     pub body: HierarchyBody,
+// }
 
-pub struct GenerateBlock {
-    pub span: Span,
-    pub label: Option<Name>,
-    pub body: HierarchyBody,
-}
+// pub struct GenerateFor {
+//     pub span: Span,
+//     pub init: ast::Stmt,
+//     pub cond: ast::Expr,
+//     pub step: ast::Expr,
+//     pub block: GenerateBlock,
+// }
 
-pub struct GenerateFor {
-    pub span: Span,
-    pub init: ast::Stmt,
-    pub cond: ast::Expr,
-    pub step: ast::Expr,
-    pub block: GenerateBlock,
-}
-
-pub struct GenerateIf {
-    pub span: Span,
-    pub cond: ast::Expr,
-    pub main_block: GenerateBlock,
-    pub else_block: Option<GenerateBlock>,
-}
+// pub struct GenerateIf {
+//     pub span: Span,
+//     pub cond: ast::Expr,
+//     pub main_block: GenerateBlock,
+//     pub else_block: Option<GenerateBlock>,
+// }
 
 /// A type.
 #[derive(Debug, Clone, PartialEq, Eq)]
