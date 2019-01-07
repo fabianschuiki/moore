fd2e742904a9dc1fc425ee6b568f44a1e6bbdfb3
Fabian Schuiki <fabian@schuiki.ch>
2019-01-07 08:49:24 +0100

    hir: update module doc

--- src/svlog/hir/mod.rs
+++ src/svlog/hir/mod.rs
@@ -1,9 +1,9 @@
 // Copyright (c) 2017 Fabian Schuiki
 
-//! The high-level intermediate representation for SystemVerilog. After name
-//! resolution, the AST is lowered into this representation, eliminating a lot
-//! of the syntactic sugar represented in the AST, and resolving default and
-//! implicit values.
+//! The high-level intermediate representation for SystemVerilog.
+//!
+//! After parsing the AST is lowered into this representation, eliminating a lot
+//! of syntactic sugar and resolving any syntactic ambiguities.
 
 mod lowering;
 mod nodes;
