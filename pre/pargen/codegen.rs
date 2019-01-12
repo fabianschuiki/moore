735a9c1c08a9f1ac0ecc8cb47624b7b813d3cb35
Fabian Schuiki <fabian@schuiki.ch>
2019-01-12 15:52:27 +0100

    drop unused parser generator
    Remove the `pre` directory which contains a legacy parser generator that
    is not used anymore.

--- pre/pargen/codegen.rs
+++
@@ -1,3 +0,0 @@
-// Copyright (c) 2016-2017 Fabian Schuiki
-use std;
-use pargen::states::{State};
--- pre/test.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016-2017 Fabian Schuiki
-use std::path::Path;
-mod pargen;
-
-
-fn main() {
-	// pargen::generate(Path::new("../src/vhdl/grammar"), Path::new("/tmp/vhdl-parser.rs"));
-	pargen::generate(Path::new("../src/svlog/grammar"), Path::new("/tmp/svlog-parser.rs"));
-}
