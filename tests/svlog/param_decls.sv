77a0f86717f1b8f12fc4bafc2175daf2148369ae
Fabian Schuiki <fabian@schuiki.ch>
2019-01-28 08:23:19 +0100

    tests: update param decls tests

--- tests/svlog/param_decls.sv
+++ tests/svlog/param_decls.sv
@@ -4,6 +4,20 @@ module A;
     int a = N;
 endmodule
 
+//@ elab A
+//| entity @A () () {
+//|     %a = sig i32 13
+//| }
+
 module B;
 	A #(42) a();
 endmodule
+
+//@ elab B
+//| entity @A.param1 () () {
+//|     %a = sig i32 43
+//| }
+//|
+//| entity @B () () {
+//|     %a = inst @A.param1 () ()
+//| }
