3f90e057769cca4b6dd27c7ab0551a289b86e2ad
Fabian Schuiki <fabian@schuiki.ch>
2019-10-01 22:47:19 +0200

    mir: intern nodes, add basic nodes
    Add arena to internalize MIR nodes in the GlobalArenas. Add the basic
    MIR nodes for the array pattern expression example in `pattern.sv`.

--- src/svlog/mir/rvalue.rs
+++ src/svlog/mir/rvalue.rs
@@ -6,6 +6,7 @@
 //! side of an assignment.
 
 use crate::{crate_prelude::*, ty::Type};
+use std::collections::HashMap;
 
 /// An rvalue expression.
 #[derive(Debug, Clone)]
@@ -15,12 +16,39 @@ pub struct Rvalue<'a> {
     /// The type of the expression.
     pub ty: Type<'a>,
     /// The expression data.
-    pub kind: RvalueKind,
+    pub kind: RvalueKind<'a>,
 }
 
 /// The different forms an rvalue expression may take.
 #[derive(Debug, Clone)]
-pub enum RvalueKind {
+#[allow(missing_docs)]
+pub enum RvalueKind<'a> {
+    /// A cast from a four-valued type to a two-valued type, or vice versa.
+    /// E.g. `integer` to `int`, or `int` to `integer`.
+    CastValueDomain {
+        from: ty::Domain,
+        to: ty::Domain,
+        value: &'a Rvalue<'a>,
+    },
+    /// A cast from a single-element vector type to an atom type.
+    /// E.g. `bit [0:0]` to `bit`.
+    CastVectorToAtom {
+        domain: ty::Domain,
+        value: &'a Rvalue<'a>,
+    },
+    /// A cast from an atom type to a single-element vector type.
+    /// E.g. `bit` to `bit [0:0]`.
+    CastAtomToVector {
+        domain: ty::Domain,
+        value: &'a Rvalue<'a>,
+    },
+    /// Shrink the width of a vector type. E.g. `bit [31:0]` to `bit [7:0]`.
+    Truncate {
+        target_width: usize,
+        value: &'a Rvalue<'a>,
+    },
+    /// Constructor for an array.
+    ConstructArray(HashMap<usize, &'a Rvalue<'a>>),
     /// An error occurred during lowering.
     Error,
 }
