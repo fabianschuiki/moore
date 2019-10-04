69430d57ae9277e8674d74c21e9ee79ff0c60b04
Fabian Schuiki <fabian@schuiki.ch>
2019-10-04 15:26:12 +0200

    mir: impl concat lowering
    Implement lowering of concatenation and repetition expressions to MIR
    rvalues.

--- tests/svlog/mir/expr.sv
+++ tests/svlog/mir/expr.sv
@@ -19,10 +19,19 @@ module a0;
 	// v3 = Zext(v2, 17) @ logic [16:0]
 	// v4 = IntBinaryArith(Add, unsigned, 17, v1, v3) @ logic [16:0]
 
-	// assign sumB = {a + b}; // addition on 16 bits, then cast to 17 bits
+	assign sumB = {a + b}; // addition on 16 bits, then cast to 17 bits
 	// v0 = Var(a) @ logic [14:0]
 	// v1 = Zext(v0, 16) @ logic [15:0]
 	// v2 = Var(b) @ logic [15:0]
 	// v3 = IntBinaryArith(Add, unsigned, 16, v1, v2) @ logic [15:0]
 	// v4 = Zext(v3, 17) @ logic [16:0]
+endmodule
+
+// 11.4.12 Concatenation operators
+module a1;
+	logic a;
+	logic [7:0] b;
+	logic w;
+	logic [8:0] z;
+	assign z = {a, b[3:0], w, 3'b101};
 endmodule
