4e9b5ba1e193d9a574fa9175b789467c18aa9030
Fabian Schuiki <fabian@schuiki.ch>
2020-04-21 13:47:53 +0200

    test: Add worst-case svlog port test case

---
+++ test/svlog/ports/nightmare.sv
@@ -0,0 +1,18 @@
+// RUN: moore %s -e foo
+// IGNORE
+
+module foo (
+    .P1(x1[3:0]),
+    .P2(x1[7:0]),
+    .P3(x2),
+    P4,
+    .P5({x3, x4}),
+    {x5, x6}
+);
+    // This language needs to die.
+    output [7:0] x1;
+    ref x2;
+    input P4;
+    input x3, x5;
+    output x4, x6;
+endmodule
