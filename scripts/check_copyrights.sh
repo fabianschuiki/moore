adddd07006f5f1376299d0f0d9f5a76f55dfd180
Fabian Schuiki <fabian@schuiki.ch>
2020-04-29 07:32:18 +0200

    scripts: Add check_copyrights

---
+++ scripts/check_copyrights.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script checks the copyrights at the beginning of source files. Emits a
+# patch to adjust the copyrights. Use as follows:
+#
+#     check_copyrights.sh | patch -p0
+
+YEAR=$(date +%Y)
+for FILE in $(find src -name "*.rs"); do
+	sed -E "s/(Copyright \(c\) )[0-9]+(-[0-9]+)?/\12016-$YEAR/g" $FILE | diff -u $FILE -
+done
