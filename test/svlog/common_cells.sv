bb39e5862c17cba46a6b9a45239fc6f38c19a76e
Fabian Schuiki <fabian@schuiki.ch>
2020-05-05 08:55:45 +0200

    test: Add stream_delay from common_cells

--- test/svlog/common_cells.sv
+++ test/svlog/common_cells.sv
@@ -1,5 +1,6 @@
 // RUN: moore %s -e foo -I ../third-party/common_cells/include ../third-party/common_cells/src/*.sv
 
 module foo;
+    stream_delay i0 ();
     stream_demux #(.N_OUP(4)) i0 ();
 endmodule
