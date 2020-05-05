e901cb5f3806ddfc85ff1b19b7de119a1dcc0390
Fabian Schuiki <fabian@schuiki.ch>
2020-05-05 10:10:41 +0200

    test: Add rr_arb_tree from common_cells

--- test/svlog/common_cells.sv
+++ test/svlog/common_cells.sv
@@ -1,6 +1,7 @@
 // RUN: moore %s -e foo -I ../third-party/common_cells/include ../third-party/common_cells/src/*.sv
 
 module foo;
+    rr_arb_tree #(.NumIn(4)) i0 ();
     stream_delay i0 ();
     stream_demux #(.N_OUP(4)) i0 ();
 endmodule
