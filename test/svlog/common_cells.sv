99e22a5e70033d6ad6ad716aae2b9f53cf880ab0
Fabian Schuiki <fabian@schuiki.ch>
2020-05-05 13:10:40 +0200

    test: Add addr_decode from common_cells

--- test/svlog/common_cells.sv
+++ test/svlog/common_cells.sv
@@ -1,6 +1,14 @@
 // RUN: moore %s -e foo -I ../third-party/common_cells/include ../third-party/common_cells/src/*.sv
 
 module foo;
+    typedef int unsigned addr_t;
+    typedef struct packed {
+      int unsigned idx;
+      addr_t start_addr;
+      addr_t end_addr;
+    } rule_t;
+
+    addr_decode #(.NoIndices(4), .NoRules(4), .addr_t(addr_t), .rule_t(rule_t)) i0 ();
     rr_arb_tree #(.NumIn(4)) i0 ();
     stream_delay i0 ();
     stream_demux #(.N_OUP(4)) i0 ();
