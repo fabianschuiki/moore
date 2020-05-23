0041dc23ddd7a666c0353f05d50b29bf9e47919b
Fabian Schuiki <fabian@schuiki.ch>
2020-05-23 12:34:56 +0200

    scripts: Update recursive publish script
    Still doesn't work properly though. It's a shame that there is no way to
    force cargo to update its index.

--- scripts/recursive_publish.sh
+++ scripts/recursive_publish.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
-(cd src/common && cargo publish "$@")
-(cd src/svlog/syntax && cargo publish "$@")
-(cd src/svlog && cargo publish "$@")
+(cd src/common && cargo publish "$@") &&
+sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
+(cd src/derive && cargo publish "$@") &&
+sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
+(cd src/svlog/syntax && cargo publish "$@") &&
+sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
+(cd src/svlog && cargo publish "$@") &&
+sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
 (cd src/vhdl/syntax && cargo publish "$@")
+sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
 (cd src/vhdl && cargo publish "$@")
+sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
 (cd src && cargo publish "$@")
