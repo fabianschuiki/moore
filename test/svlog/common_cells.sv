9ae26423d2fab9404f9fc98e4e3f8d31554b8c2a
Fabian Schuiki <fabian@schuiki.ch>
2020-05-30 12:34:43 +0200

    test: Enumerate instances properly

--- test/svlog/common_cells.sv
+++ test/svlog/common_cells.sv
@@ -9,56 +9,56 @@ module foo;
     } rule_t;
 
     addr_decode #(.NoIndices(4), .NoRules(4), .addr_t(addr_t), .rule_t(rule_t)) i0();
-    // cb_filter i0();
-    cdc_2phase #(.T(logic [41:0])) i0();
-    cdc_fifo_2phase #(.T(logic [41:0])) i0();
-    cdc_fifo_gray #(.T(logic [41:0])) i0();
-    clk_div i0();
-    counter i0();
-    delta_counter i0();
-    // ecc_decode i0();
-    // ecc_encode i0();
-    // edge_detect i0(); // broken in repo
-    // edge_propagator_rx i0(); // broken in repo
-    // edge_propagator i0(); // broken in repo
-    edge_propagator_tx i0();
-    exp_backoff i0();
-    // fall_through_register i0(); // broken in repo
-    fifo_v3 i0();
-    binary_to_gray #(.N(9)) i0();
-    gray_to_binary #(.N(9)) i0();
-    // id_queue #(.ID_WIDTH(2), .CAPACITY(3), .data_t(logic [41:0])) i0();
-    lfsr_16bit i0();
-    lfsr_8bit i0();
-    // lfsr i0();
-    lzc i0();
-    max_counter i0();
-    mv_filter i0();
-    onehot_to_bin i0();
-    // plru_tree i0();
-    popcount #(.INPUT_WIDTH(16)) i0();
-    rr_arb_tree #(.NumIn(4)) i0();
-    rstgen_bypass i0();
-    rstgen i0();
-    serial_deglitch i0();
-    shift_reg #(.Depth(0)) i0();
-    shift_reg #(.Depth(1)) i0();
-    shift_reg #(.Depth(4)) i0();
-    spill_register i0();
-    sram #(.NUM_WORDS(16)) i0();
-    // stream_arbiter_flushable #(.N_INP(4)) i0();
-    // stream_arbiter #(.N_INP(4)) i0();
-    stream_delay i0();
-    stream_demux #(.N_OUP(4)) i0();
-    stream_fifo i0();
-    stream_filter i0();
-    stream_fork_dynamic #(.N_OUP(4)) i0();
-    stream_fork #(.N_OUP(4)) i0();
-    stream_join #(.N_INP(4)) i0();
-    stream_mux #(.N_INP(4)) i0();
-    // stream_register i0(); // broken in repo
-    // sub_per_hash i0();
-    sync i0();
-    // sync_wedge i0(); // broken in repo
-    unread i0();
+    // cb_filter i1();
+    cdc_2phase #(.T(logic [41:0])) i2();
+    cdc_fifo_2phase #(.T(logic [41:0])) i3();
+    cdc_fifo_gray #(.T(logic [41:0])) i4();
+    clk_div i5();
+    counter i6();
+    delta_counter i7();
+    // ecc_decode i8();
+    // ecc_encode i9();
+    // edge_detect i10(); // broken in repo
+    // edge_propagator_rx i11(); // broken in repo
+    // edge_propagator i12(); // broken in repo
+    edge_propagator_tx i13();
+    exp_backoff i14();
+    // fall_through_register i15(); // broken in repo
+    fifo_v3 i16();
+    binary_to_gray #(.N(9)) i17();
+    gray_to_binary #(.N(9)) i18();
+    // id_queue #(.ID_WIDTH(2), .CAPACITY(3), .data_t(logic [41:0])) i19();
+    lfsr_16bit i20();
+    lfsr_8bit i21();
+    // lfsr i22();
+    lzc i23();
+    max_counter i24();
+    mv_filter i25();
+    onehot_to_bin i26();
+    // plru_tree i27();
+    popcount #(.INPUT_WIDTH(16)) i28();
+    rr_arb_tree #(.NumIn(4)) i29();
+    rstgen_bypass i30();
+    rstgen i31();
+    serial_deglitch i32();
+    shift_reg #(.Depth(0)) i33();
+    shift_reg #(.Depth(1)) i34();
+    shift_reg #(.Depth(4)) i35();
+    spill_register i36();
+    sram #(.NUM_WORDS(16)) i37();
+    // stream_arbiter_flushable #(.N_INP(4)) i38();
+    // stream_arbiter #(.N_INP(4)) i39();
+    stream_delay i40();
+    stream_demux #(.N_OUP(4)) i41();
+    stream_fifo i42();
+    stream_filter i43();
+    stream_fork_dynamic #(.N_OUP(4)) i44();
+    stream_fork #(.N_OUP(4)) i45();
+    stream_join #(.N_INP(4)) i46();
+    stream_mux #(.N_INP(4)) i47();
+    // stream_register i48(); // broken in repo
+    // sub_per_hash i49();
+    sync i50();
+    // sync_wedge i51(); // broken in repo
+    unread i52();
 endmodule
--- test/svlog/exprs/const.sv
+++ test/svlog/exprs/const.sv
@@ -13,168 +13,168 @@ module foo;
 
     bar #(+a) i0();
     // CHECK: %0 = const i32 42
-    bar #(-a) i0();
+    bar #(-a) i1();
     // CHECK: %0 = const i32 4294967254
-    bar #(~a) i0();
+    bar #(~a) i2();
     // CHECK: %0 = const i32 4294967253
     // TODO: Fix these to i32 once #141 lands.
-    bar #(!a) i0();
-    bar #(!z) i0();
+    bar #(!a) i3();
+    bar #(!z) i4();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(&z) i0();
-    bar #(&w) i0();
-    bar #(&x) i0();
+    bar #(&z) i5();
+    bar #(&w) i6();
+    bar #(&x) i7();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(~&z) i0();
-    bar #(~&w) i0();
-    bar #(~&x) i0();
+    bar #(~&z) i8();
+    bar #(~&w) i9();
+    bar #(~&x) i10();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(|z) i0();
-    bar #(|w) i0();
-    bar #(|x) i0();
+    bar #(|z) i11();
+    bar #(|w) i12();
+    bar #(|x) i13();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 1
-    bar #(~|z) i0();
-    bar #(~|w) i0();
-    bar #(~|x) i0();
+    bar #(~|z) i14();
+    bar #(~|w) i15();
+    bar #(~|x) i16();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 0
-    bar #(^a) i0();
-    bar #(^b) i0();
+    bar #(^a) i17();
+    bar #(^b) i18();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(~^a) i0();
-    bar #(~^b) i0();
+    bar #(~^a) i19();
+    bar #(~^b) i20();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(^~a) i0();
-    bar #(^~b) i0();
+    bar #(^~a) i21();
+    bar #(^~b) i22();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
 
     // Binary Operators
 
-    bar #(a + b) i0();
+    bar #(a + b) i23();
     // CHECK: %0 = const i32 51
-    bar #(a - b) i0();
+    bar #(a - b) i24();
     // CHECK: %0 = const i32 33
-    bar #(a * b) i0();
+    bar #(a * b) i25();
     // CHECK: %0 = const i32 378
-    bar #(a / b) i0();
+    bar #(a / b) i26();
     // CHECK: %0 = const i32 4
-    bar #(a % b) i0();
+    bar #(a % b) i27();
     // CHECK: %0 = const i32 6
-    bar #(a ** c) i0();
+    bar #(a ** c) i28();
     // CHECK: %0 = const i32 74088
     // TODO: Fix these to i32 once #141 lands.
-    bar #(a == a) i0();
-    bar #(a == b) i0();
+    bar #(a == a) i29();
+    bar #(a == b) i30();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(a === a) i0();
-    bar #(a === b) i0();
+    bar #(a === a) i31();
+    bar #(a === b) i32();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(a ==? a) i0();
-    bar #(a ==? b) i0();
+    bar #(a ==? a) i33();
+    bar #(a ==? b) i34();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(a != a) i0();
-    bar #(a != b) i0();
+    bar #(a != a) i35();
+    bar #(a != b) i36();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(a !== a) i0();
-    bar #(a !== b) i0();
+    bar #(a !== a) i37();
+    bar #(a !== b) i38();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(a !=? a) i0();
-    bar #(a !=? b) i0();
+    bar #(a !=? a) i39();
+    bar #(a !=? b) i40();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(a < a) i0();
-    bar #(a < b) i0();
-    bar #(b < a) i0();
+    bar #(a < a) i41();
+    bar #(a < b) i42();
+    bar #(b < a) i43();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(a <= a) i0();
-    bar #(a <= b) i0();
-    bar #(b <= a) i0();
+    bar #(a <= a) i44();
+    bar #(a <= b) i45();
+    bar #(b <= a) i46();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
-    bar #(a > a) i0();
-    bar #(a > b) i0();
-    bar #(b > a) i0();
+    bar #(a > a) i47();
+    bar #(a > b) i48();
+    bar #(b > a) i49();
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(a >= a) i0();
-    bar #(a >= b) i0();
-    bar #(b >= a) i0();
+    bar #(a >= a) i50();
+    bar #(a >= b) i51();
+    bar #(b >= a) i52();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
-    bar #(a << b) i0();
+    bar #(a << b) i53();
     // CHECK: %0 = const i32 21504
-    bar #(a <<< b) i0();
+    bar #(a <<< b) i54();
     // CHECK: %0 = const i32 21504
-    bar #(d >> b) i0();
-    bar #(w >> b) i0();
+    bar #(d >> b) i55();
+    bar #(w >> b) i56();
     // CHECK: %0 = const i32 24112
     // CHECK: %0 = const i32 8388607
-    bar #(d >>> b) i0();
-    bar #(w >>> b) i0();
+    bar #(d >>> b) i57();
+    bar #(w >>> b) i58();
     // CHECK: %0 = const i32 24112
     // TODO: Fix this once #142 lands
     //!CHECK: %0 = const i32 4294967295
-    bar #(x & y) i0();
-    bar #(x ~& y) i0();
+    bar #(x & y) i59();
+    bar #(x ~& y) i60();
     // CHECK: %0 = const i32 132
     // CHECK: %0 = const i32 4294967163
-    bar #(x | y) i0();
-    bar #(x ~| y) i0();
+    bar #(x | y) i61();
+    bar #(x ~| y) i62();
     // CHECK: %0 = const i32 237
     // CHECK: %0 = const i32 4294967058
-    bar #(x ^ y) i0();
-    bar #(x ~^ y) i0();
-    bar #(x ^~ y) i0();
+    bar #(x ^ y) i63();
+    bar #(x ~^ y) i64();
+    bar #(x ^~ y) i65();
     // CHECK: %0 = const i32 105
     // CHECK: %0 = const i32 4294967190
     // CHECK: %0 = const i32 4294967190
-    bar #(a && b) i0();
-    bar #(z && b) i0();
-    bar #(z && z) i0();
+    bar #(a && b) i66();
+    bar #(z && b) i67();
+    bar #(z && z) i68();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
     // CHECK: %0 = const i32 0
-    bar #(a || b) i0();
-    bar #(z || b) i0();
-    bar #(z || z) i0();
+    bar #(a || b) i69();
+    bar #(z || b) i70();
+    bar #(z || z) i71();
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 1
     // CHECK: %0 = const i32 0
 
     // Ternary Operator
 
-    bar #(a == b ? 42 : 9001) i0();
-    bar #(a != b ? 42 : 9001) i0();
+    bar #(a == b ? 42 : 9001) i72();
+    bar #(a != b ? 42 : 9001) i73();
     // CHECK: %0 = const i32 9001
     // CHECK: %0 = const i32 42
 
     // Cast Operators
 
-    bar #(unsigned'(a)) i0();
-    bar #(signed'(a)) i0();
-    bar #($unsigned(a)) i0();
-    bar #($signed(a)) i0();
+    bar #(unsigned'(a)) i74();
+    bar #(signed'(a)) i75();
+    bar #($unsigned(a)) i76();
+    bar #($signed(a)) i77();
     // CHECK: %0 = const i32 42
     // CHECK: %0 = const i32 42
     // CHECK: %0 = const i32 42
