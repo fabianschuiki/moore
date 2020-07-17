cd51795b74eb7be12f9783d31a5efd7d7fb5bb29
Paulo Henrique Silva <ph.silva@carta.com>
2020-07-17 12:05:45 -0300

    svlog: add test for drive strength fix

---
+++ test/svlog/parser/drive_strength.sv
@@ -0,0 +1,10 @@
+// RUN: moore %s -e foo -O0
+module foo;
+    wire (highz1, pull0) bar;
+endmodule
+
+// CHECK: entity @foo () -> () {
+// CHECK:     %0 = const i1 0
+// CHECK:     %bar = sig i1 %0
+// CHECK:     halt
+// CHECK: }
