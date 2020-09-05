2e95d615199d412673d0e7563ebc10eca10fc68c
Fabian Schuiki <fabian@schuiki.ch>
2020-09-05 15:53:33 +0200

    scripts: Update publication script

--- scripts/recursive_publish.sh
+++ scripts/recursive_publish.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -e
 (cd src/common && cargo publish "$@") &&
-sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
 (cd src/derive && cargo publish "$@") &&
-sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
+sleep 30 && (cargo install lazy_static > /dev/null 2>&1 || true)
 (cd src/svlog/syntax && cargo publish "$@") &&
-sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
-(cd src/svlog && cargo publish "$@") &&
-sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
 (cd src/vhdl/syntax && cargo publish "$@")
-sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
+sleep 30 && (cargo install lazy_static > /dev/null 2>&1 || true)
+(cd src/svlog && cargo publish "$@") &&
 (cd src/vhdl && cargo publish "$@")
-sleep 10 && (cargo install lazy_static > /dev/null 2>&1 || true)
+sleep 30 && (cargo install lazy_static > /dev/null 2>&1 || true)
 cargo publish "$@"
