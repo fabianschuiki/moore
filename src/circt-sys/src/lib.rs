1ff86d48b29a840fa776ee362cd54f3eedc7c51e
Fabian Schuiki <fabian@schuiki.ch>
2021-11-15 10:30:33 +0100

    mlir: Add helper to go from MlirStringRef to &str

--- src/circt-sys/src/lib.rs
+++ src/circt-sys/src/lib.rs
@@ -11,3 +11,11 @@ pub unsafe fn mlirStringRefCreateFromStr(s: impl AsRef<str>) -> MlirStringRef {
         length: s.len() as size_t,
     }
 }
+
+pub unsafe fn mlirStringRefToStr<R>(s: MlirStringRef, f: impl Fn(&str) -> R) -> R {
+    f(std::str::from_utf8(std::slice::from_raw_parts(
+        s.data as *const _,
+        s.length as usize,
+    ))
+    .expect("utf8 string"))
+}
