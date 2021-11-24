5a35b81da57abfa42821cfea8b83f0a47575f718
Fabian Schuiki <fabian@schuiki.ch>
2021-11-24 15:14:43 +0100

    mlir: Point CI build to install directories instead of build

--- .github/build-circt.sh
+++ .github/build-circt.sh
@@ -7,8 +7,8 @@ cd circt/build
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=../install \
-    -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
-    -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
+    -DMLIR_DIR=$PWD/../llvm/install/lib/cmake/mlir \
+    -DLLVM_DIR=$PWD/../llvm/install/lib/cmake/llvm \
     -DLLVM_ENABLE_ASSERTIONS=ON
 
 cmake --build . --target install -- -j$(nproc)
